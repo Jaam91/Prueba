3972461573785e804f7b3d579d1ee17e3f18d137
Jaam91 <JohnyIECI@yahoo.cl>
2015-04-28 11:54:38 -0300

    Comentario script implemento.js
    Comentando script para hacer más entendible el código

--- gimnasio/js/implemento.js
+++ gimnasio/js/implemento.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
         
-        $('#otro').hide();
+        $('#otro').hide(); //Ocultamos la etiqueta al principio
     
-    $("select[name='Implemento\\[grupo_muscular\\]']").change(function(){
+    $("select[name='Implemento\\[grupo_muscular\\]']").change(function(){//Comienza la función
 
-        if ($(this).val()== '-Otro-') { 
-
+        if ($(this).val()== '-Otro-') { //Si elegimos -Otro-, nos mostrará el div con el id otro para agregar un nuevo grupo muscular
              $('#otro').show();
 
         }else{
-
             $('#otro').hide();
         }
         
